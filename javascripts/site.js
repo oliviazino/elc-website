a9410a0fa151263c52c0dc1aebcbe91892c8289c
Ray Kman <rkman@nd.edu>
2020-08-05 01:24:57 -0400

    Going with Original Council Design For Now

--- javascripts/site.js
+++ javascripts/site.js
@@ -92,4 +92,22 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelectorAll('iframe[data-init="false"]')[0].setAttribute('data-init', true);
   }
 
+
+  var modal = document.querySelector(".modal");
+  var trigger = document.querySelector(".trigger");
+  var closeButton = document.querySelector(".close-button");
+
+  function toggleModal() {
+      modal.classList.toggle("show-modal");
+  }
+
+  function windowOnClick(event) {
+      if (event.target === modal) {
+          toggleModal();
+      }
+  }
+
+  trigger.addEventListener("click", toggleModal);
+  closeButton.addEventListener("click", toggleModal);
+  window.addEventListener("click", windowOnClick);
 });
